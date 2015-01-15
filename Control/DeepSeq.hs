haskell/deepseq

commit de1bc894de1ffdd34e6eb8be4fb9e057198060c6

Remove redundant constraints, discovered by -fwarn-redundant-constraints

Control/DeepSeq.hs
@@ -284,10 +284,10 @@ instance NFData (a -> b) where rnf !_ = ()
 
 --Rational and complex numbers.
 
-instance (Integral a, NFData a) => NFData (Ratio a) where
+instance NFData a => NFData (Ratio a) where
   rnf x = rnf (numerator x, denominator x)
 
-instance (RealFloat a, NFData a) => NFData (Complex a) where
+instance (NFData a) => NFData (Complex a) where
   rnf (x:+y) = rnf x `seq`
                rnf y `seq`
                ()
@@ -316,7 +316,7 @@ instance NFData a => NFData (ZipList a) where
 instance NFData a => NFData (Const a b) where
     rnf = rnf . getConst
 
-instance (Ix a, NFData a, NFData b) => NFData (Array a b) where
+instance (NFData a, NFData b) => NFData (Array a b) where
     rnf x = rnf (bounds x, Data.Array.elems x)
 
 #if MIN_VERSION_base(4,6,0)

commit 5077f36c1422c70cf426b2466d12a3c5476040cc

Restore compatibility with base-4.3
This is fallout from de1bc89

Control/DeepSeq.hs
@@ -291,7 +291,11 @@ instance (Integral a, NFData a) => NFData (Ratio a) where
 #endif
   rnf x = rnf (numerator x, denominator x)
 
+#if MIN_VERSION_base(4,4,0)
 instance (NFData a) => NFData (Complex a) where
+#else
+instance (RealFloat a, NFData a) => NFData (Complex a) where
+#endif
   rnf (x:+y) = rnf x `seq`
                rnf y `seq`
                ()