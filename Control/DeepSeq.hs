haskell/deepseq

commit de1bc894de1ffdd34e6eb8be4fb9e057198060c6

Remove redundant constraints, discovered by -fwarn-redundant-constraints

Control/DeepSeq.hs
@@ -284,10 +284,10 @@ instance NFData (a -> b) where rnf !_ = ()
 
 --Rational and complex numbers.
 
-instance (Integral a, NFData a) => NFData (Ratio a) where
+instance NFData a => NFData (Ratio a) where
   rnf x = rnf (numerator x, denominator x)
 
-instance (RealFloat a, NFData a) => NFData (Complex a) where
+instance (NFData a) => NFData (Complex a) where
   rnf (x:+y) = rnf x `seq`
                rnf y `seq`
                ()
@@ -316,7 +316,7 @@ instance NFData a => NFData (ZipList a) where
 instance NFData a => NFData (Const a b) where
     rnf = rnf . getConst
 
-instance (Ix a, NFData a, NFData b) => NFData (Array a b) where
+instance (NFData a, NFData b) => NFData (Array a b) where
     rnf x = rnf (bounds x, Data.Array.elems x)
 
 #if MIN_VERSION_base(4,6,0)

commit 153520ab033fd26beb47757cc6512f177cb1ae8f

Recover breakage for GHC<7.11 caused by de1bc89
de1bc89 dropped a few redundant constraints, some of which are not yet
redundant in prior GHC/base versions.

Control/DeepSeq.hs
@@ -284,7 +284,11 @@ instance NFData (a -> b) where rnf !_ = ()
 
 --Rational and complex numbers.
 
+#if __GLASGOW_HASKELL__ >= 711
 instance NFData a => NFData (Ratio a) where
+#else
+instance (Integral a, NFData a) => NFData (Ratio a) where
+#endif
   rnf x = rnf (numerator x, denominator x)
 
 instance (NFData a) => NFData (Complex a) where
@@ -316,7 +320,11 @@ instance NFData a => NFData (ZipList a) where
 instance NFData a => NFData (Const a b) where
     rnf = rnf . getConst
 
+#if __GLASGOW_HASKELL__ >= 711
 instance (NFData a, NFData b) => NFData (Array a b) where
+#else
+instance (Ix a, NFData a, NFData b) => NFData (Array a b) where
+#endif
     rnf x = rnf (bounds x, Data.Array.elems x)
 
 #if MIN_VERSION_base(4,6,0)